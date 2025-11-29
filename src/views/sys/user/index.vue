32b46f7335db3b780a2605bc5bec90fb22f007df
srd19306337595 <tanghb3@chinatelecom.cn>
2025-11-29 17:36:50 +0800

    25.11.29

--- src/views/sys/user/index.vue
+++ src/views/sys/user/index.vue
@@ -1,25 +1,90 @@
 <template>
     <div class="app-container">
 
-        <el-table :data="tableData" stripe style="width: 100%">
-            <el-table-column prop="username" label="用户名" width="180"/>
+        <el-row :gutter="20" class="header">
+            <el-col :span="7">
+                <el-input placeholder="请输入用户名..." v-model="queryForm.query" clearable></el-input>
+            </el-col>
+            <el-button type="primary" :icon="Search" @click="initUserList">搜索</el-button>
+            <el-button type="success" :icon="DocumentAdd" @click="handleDialogValue()">新增</el-button>
+            <el-popconfirm title="您确定批量删除这些记录吗？" @confirm="handleDelete(null)">
+                <template #reference>
+                    <el-button type="danger" :disabled="delBtnStatus" :icon="Delete">批量删除</el-button>
+                </template>
+            </el-popconfirm>
+        </el-row>
+
+        <el-table :data="tableData" stripe style="width: 100%" @selection-change="handleSelectionChange">
+            <el-table-column type="selection" width="55"/>
+            <el-table-column prop="avatar" label="头像" width="80" align="center">
+                <template v-slot="scope">
+                    <img :src="getServerUrl()+'media/userAvatar/'+scope.row.avatar" width="50" height="50"/>
+                </template>
+            </el-table-column>
+            <el-table-column prop="username" label="用户名" width="100" align="center"/>
+            <el-table-column prop="roles" label="拥有角色" width="200" align="center">
+                <template v-slot="scope">
+                    <el-tag size="small" type="warning" v-for="item in scope.row.roleList"> {{ item.name }}</el-tag>
+                </template>
+            </el-table-column>
+            <el-table-column prop="email" label="邮箱" width="200" align="center"/>
+            <el-table-column prop="phonenumber" label="手机号" width="120" align="center"/>
+            <el-table-column prop="status" label="状态？" width="200" align="center">
+                <template v-slot="{row}">
+                    <el-switch v-model="row.status" @change="statusChangeHandle(row)" active-text="正常"
+                               inactive-text="禁用" :active-value="1" :inactive-value="0"></el-switch>
+                </template>
+            </el-table-column>
+            <el-table-column prop="create_time" label="创建时间" width="200" align="center"/>
+            <el-table-column prop="login_date" label="最后登录时间" width="200" align="center"/>
+            <el-table-column prop="remark" label="备注"/>
+            <el-table-column prop="action" label="操作" width="400" fixed="right" align="center">
+                <template v-slot="scope">
+                    <el-button type="primary" :icon="Tools"
+                               @click="handleRoleDialogValue(scope.row.id,scope.row.roleList)">分配角色
+                    </el-button>
+
+                    <el-popconfirm v-if="scope.row.username!='python222'" title="您确定要对这个用户重置密码吗？"
+                                   @confirm="handleResetPassword(scope.row.id)">
+                        <template #reference>
+                            <el-button type="warning" :icon="RefreshRight">重置密码</el-button>
+                        </template>
+                    </el-popconfirm>
+
+                    <el-button type="primary" v-if="scope.row.username!='python222'" :icon="Edit"
+                               @click="handleDialogValue(scope.row.id)"></el-button>
+                    <el-popconfirm v-if="scope.row.username!='python222'" title="您确定要删除这条记录吗？"
+                                   @confirm="handleDelete(scope.row.id)">
+                        <template #reference>
+                            <el-button type="danger" :icon="Delete"/>
+                        </template>
+                    </el-popconfirm>
+
+
+                </template>
+            </el-table-column>
 
         </el-table>
         <el-pagination
                 v-model:currentPage="queryForm.pageNum"
                 v-model:page-size="queryForm.pageSize"
-                :page-sizes="[5, 10, 20, 30, 40]"
+                :page-sizes="[10, 20, 30, 40]"
                 layout="total, sizes, prev, pager, next, jumper"
                 :total="total"
                 @size-change="handleSizeChange"
                 @current-change="handleCurrentChange"
         />
+        <Dialog v-model="dialogVisible" :dialogVisible="dialogVisible" :id="id" :dialogTitle="dialogTitle"
+                @initUserList="initUserList"></Dialog>
     </div>
 </template>
 
 <script setup>
-import requestUtil from "@/util/request";
+import {Search, Delete, DocumentAdd, Edit, Tools, RefreshRight} from '@element-plus/icons-vue'
+import requestUtil, {getServerUrl} from "@/util/request";
 import {ref} from 'vue'
+import Dialog from './components/dialog'
+import {ElMessage} from 'element-plus'
 
 const tableData = ref([]);
 
@@ -30,6 +95,23 @@ const queryForm = ref({
     pageNum: 1,
     pageSize: 10
 })
+
+const dialogVisible = ref(false)
+
+const dialogTitle = ref("")
+
+const id = ref(-1)
+
+const handleDialogValue = (userId) => {
+    if (userId) {
+        id.value = userId;
+        dialogTitle.value = "用户修改"
+    } else {
+        id.value = -1;
+        dialogTitle.value = "用户添加"
+    }
+    dialogVisible.value = true
+}
 
 const initUserList = async () => {
     const res = await requestUtil.post("user/search", queryForm.value);
@@ -50,6 +132,74 @@ const handleCurrentChange = (pageNum) => {
     initUserList()
 }
 
+const delBtnStatus = ref(true)
+
+const multipleSelection = ref([])
+
+const handleSelectionChange = (selection) => {
+    console.log("勾选了")
+    console.log(selection)
+    multipleSelection.value = selection;
+    delBtnStatus.value = selection.length == 0;
+}
+
+const handleDelete = async (id) => {
+    var ids = []
+    if (id) {
+        ids.push(id)
+    } else {
+        multipleSelection.value.forEach(row => {
+            ids.push(row.id)
+        })
+    }
+    const res = await requestUtil.del("user/action", ids)
+    if (res.data.code == 200) {
+        ElMessage({
+            type: 'success',
+            message: '执行成功!'
+        })
+        initUserList();
+    } else {
+        ElMessage({
+            type: 'error',
+            message: res.data.msg,
+        })
+    }
+
+}
+
+const handleResetPassword = async (id) => {
+    const res = await requestUtil.get("user/resetPassword?id=" + id)
+    if (res.data.code == 200) {
+        ElMessage({
+            type: 'success',
+            message: '执行成功!'
+        })
+        initUserList();
+    } else {
+        ElMessage({
+            type: 'error',
+            message: res.data.msg,
+        })
+    }
+}
+
+const statusChangeHandle = async (row) => {
+    let res = await requestUtil.post("user/status", {id: row.id, status: row.status});
+    if (res.data.code == 200) {
+        ElMessage({
+            type: 'success',
+            message: '执行成功!'
+        })
+    } else {
+        ElMessage({
+            type: 'error',
+            message: res.data.msg,
+        })
+        initUserList();
+    }
+}
+
 </script>
 
 <style lang="scss" scoped>
@@ -65,7 +215,7 @@ const handleCurrentChange = (pageNum) => {
   box-sizing: border-box;
 }
 
-::v-deep th.el-table__cell {
+:deep(th.el-table__cell) {
   word-break: break-word;
   background-color: #f8f8f9 !important;
   color: #515a6e;
